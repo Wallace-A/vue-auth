b17199882a1702940441ec066fcd9fc6eb0c35c1
Wallace-A <andrew.wallace.english@gmail.com>
2021-10-07 13:47:24 +0900

    firebase auth and bugfix

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,31 @@
   <router-view/>
 </template>
 
+<script>
+import { onBeforeMount } from "vue";
+import { useRouter, useRoute} from "vue-router";
+import firebase from "firebase/compat";
+import 'firebase/auth';
+
+export default {
+  setup () {
+    const router = useRouter();
+    const route = useRoute();
+
+    onBeforeMount(() => {
+      firebase.auth().onAuthStateChanged((user) => {
+        //if no user then go to login, else go to home page 
+        if(!user) {
+          router.replace("/login");
+        } else if (route.path == "/login" || route.path == "/register") {
+          router.replace("/");
+        }
+      })
+    })
+  }
+}
+</script>
+
 <style lang="scss">
 body {
   background: #2c3e50;
